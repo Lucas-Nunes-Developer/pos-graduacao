713fd91bc0e64a2bdc81d9bec7d83498fd992bf2
Lucas Nunes <lucasnunespinto@hotmail.com>
2025-01-13 12:04:36 -0300

    Ajusted: Funções

--- estudos-de-front-end/javascript/funcoes.js
+++ estudos-de-front-end/javascript/funcoes.js
@@ -1,60 +1,43 @@
-function validarEntrada(n1, n2) {
-    if (n1 === "" || isNaN(n1)) {
-        alert("Por favor, insira um número válido em N1.");
-        return false;
-    }
-    if (n2 === "" || isNaN(n2)) {
-        alert("Por favor, insira um número válido em N2.");
-        return false;
-    }
-    return true;
+function calcular(operacao) {
+  const n1 = document.getElementById("n1").value;
+  const n2 = document.getElementById("n2").value;
+
+  let resultado;
+  switch (operacao) {
+    case "+":
+      resultado = somar(n1, n2);
+      break;
+    case "-":
+      resultado = subtracao(n1, n2);
+      break;
+    case "*":
+      resultado = multiplicacao(n1, n2);
+      break;
+    case "/":
+      resultado = divisao(n1, n2);
+      break;
+  }
+
+  document.getElementById("resultado").innerHTML = "O resultado é " + resultado;
 }
 
-function somar(){
-    const n1 = document.getElementById('n1').value
-    const n2 = document.getElementById('n2').value
-
-    if (!validarEntrada(n1, n2)) return;
-
-    const soma = Number(n1) + Number(n2)
-
-    document.getElementById('resultadoSoma').innerHTML = "A soma dos números é " + soma
+function somar(n1, n2) {
+  return Number(n1) + Number(n2);
 }
 
-function subtracao(){
-    const n1 = document.getElementById('n1').value
-    const n2 = document.getElementById('n2').value
-
-    if (!validarEntrada(n1, n2)) return;
-
-    const sub = Number(n1) - Number(n2)
-
-    document.getElementById('resultadoSubtracao').innerHTML = "A subtração dos números é " + sub
+function subtracao(n1, n2) {
+  return Number(n1) - Number(n2);
 }
 
-function multiplicacao(){
-    const n1 = document.getElementById('n1').value
-    const n2 = document.getElementById('n2').value
-
-    if (!validarEntrada(n1, n2)) return;
-
-    const multi = Number(n1) * Number(n2)
-
-    document.getElementById('resultadoMultiplicacao').innerHTML = "A multiplicação dos números é " + multi
+function multiplicacao(n1, n2) {
+  return Number(n1) * Number(n2);
 }
 
-function divisao(){
-    const n1 = document.getElementById('n1').value
-    const n2 = document.getElementById('n2').value
-
-    if (!validarEntrada(n1, n2)) return;
-
-    if (parseFloat(n2) === 0) {
-        alert("Divisão por zero não é permitida.");
-        return;
-    }
-
-    const devi = Number(n1) / Number(n2)
-
-    document.getElementById('resultadoDivisao').innerHTML = "A divisão dos números é " + devi
+function divisao(n1, n2) {
+  if (parseFloat(n2) === 0) {
+    console.error("Divisão por zero não é permitida.");
+    return null;
+  } else {
+    return Number(n1) / Number(n2);
+  }
 }
